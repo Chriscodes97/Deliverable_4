741f1e14494a232f0e4d1fecc8234febfa39e43b
VRaid <VRaid@ZEUS>
2023-02-08 18:11:02 -0500

    Add project files.

---
+++ Deliverable_4/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+  Program Author: Chris Parrado
+  Date: 2/7/23
+  Program description: C# application that demonstrate the use of arrays after getting input from users.
+*/
+using System;
+namespace Deliverable_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[25];
+
+            // Intitalize the fibonacci array
+            fibonacci[0] = 0;
+
+            fibonacci[1] = 1;
+
+
+            for (int i = 2; i < fibonacci.Length; i++)
+            {
+                    
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                
+            }
+
+            for (int i = 0; i < fibonacci.Length; i++)
+            {
+                Console.WriteLine("Fibonacci (" + i + ") = " + fibonacci[i]);
+            }
+
+        }
+    }
+}
